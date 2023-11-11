b74b8bbc1d262c6323ece9a50d787c6e635906ca
agnunez <anunez@gmail.com>
2023-11-11 08:08:43 +0100

    Tsky, Dewpoint and limits
    Many upgrades for Tsky corrections, dewpoint calculations, safety limits configuration, elements and overall safetry and delays for open/close roofs

--- obsafety.ino
+++ obsafety.ino
@@ -5,8 +5,12 @@
  * Based on ESP32, MLX90614 and BME280 hardware 
  * connected by I2C bus
  * Using wifi with REST api
- * (c) agnuca 2023
+ * (c) agnuca 2023 
  * 
+ * Cold Temperature Correction of Cloud Sensor:
+ * https://lunaticoastro.com/aagcw/TechInfo/SkyTemperatureModel.pdf
+ * DewPoint formula : 
+ * https://iridl.ldeo.columbia.edu/dochelp/QA/Basic/dewpoint.html
  *****************************************************/
 #include <Wire.h>
 #include <Adafruit_BME280.h>
@@ -26,17 +30,42 @@ unsigned long lastTimeRan;
 
 WebServer server(80);
 
+// temperature calculations
+
 float temperature;
 float humidity;
 float pressure;
 float tempamb;
 float tempobj;
+float tempsky;
+float dewpoint;
+float limit_tamb = 0;     // freezing below this
+float limit_tsky = -15;   // cloudy above this
+float limit_humid = 85;   // risk for electronics above this
+float limit_dew = 5;      // risk for optics with temp - dewpoint below this
+float time2open = 1200;   // waiting time before open roof after a safety close
+float time2close = 120;   // waiting time before close roof with continuos overall safety waring for this
 
-// limits
+bool  status_tamb, status_tsky, status_humid, status_dew, status_weather, status_roof;
 
-float limit_tamb = 0;
-float limit_tsky = -15;
-float limit_humid = 85;
+#define sgn(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0))          // missing math function sign of number
+
+static float k[] = {0., 33., 0., 4., 100., 100., 0., 0.};  // sky temperature corrections polynomial coefficients
+
+float tsky_calc(float ts, float ta){
+  float t67,td = 0;
+  if (abs(k[2]/10.-ta) < 1) {
+    t67=sgn(k[6])*sgn(ta-k[2]/10.) * abs((k[2]/10. - ta));
+  } else {
+    t67=k[6]*sgn(ta-k[2]/10.)*(log(abs((k[2]/10-ta)))/log(10.) + k[7]/100);
+  }
+  td = (k[1]/100.)*(ta-k[2]/10.)+(k[3]/100.)*pow(exp(k[4]/1000.*ta),(k[5]/100.))+t67;
+  return (ts-td);
+}
+
+float dewpoint_calc(float temp, float humid){
+  return (temp -((100-humid)/5.));
+}
 
 StaticJsonDocument<1024> jsonDocument;  // REST API server configuration
 
@@ -67,11 +96,20 @@ void addJsonObject(char *name, float value, char *unit, bool comma) {
 void getValues() {
   if(DEBUG) Serial.println("Get all values");
   json_str ="";
-  addJsonObject("temperature", temperature, "°C", true);
-  addJsonObject("humidity", humidity, "%", true);
-  addJsonObject("pressure", pressure, "hPa", true);
-  addJsonObject("tempamb", tempamb, "°C", true);
-  addJsonObject("tempobj", tempobj, "°C", false);
+  addJsonObject("Temperature", temperature, "°C", true);
+  addJsonObject("Humidity", humidity, "%", true);
+  addJsonObject("Pressure", pressure, "hPa", true);
+  addJsonObject("MLX temp ", tempamb, "°C", true);
+  addJsonObject("IR temp", tempobj, "°C", true);
+  addJsonObject("Sky Temp", tempsky, "°C", true);
+  addJsonObject("Dew Point", dewpoint, "°C", true);
+  addJsonObject("Temperature Safety", status_tamb, "Boolean", true);
+  addJsonObject("Cloud Safety", status_tsky, "Boolean", true);
+  addJsonObject("Humidity Safety", status_humid, "Boolean", true);
+  addJsonObject("Dew Safety", status_dew, "Boolean", true);
+  addJsonObject("Weather Overall Safety", status_weather, "Boolean", true);
+  addJsonObject("Time to open roof", time2open, "sec", true);
+  addJsonObject("Time to close roof", time2close, "sec", false);
   String sensors = "{ \"sensors\": [" + json_str + "] }";
   server.send(200, "application/json", sensors);
 }
@@ -148,6 +186,40 @@ void readSensors(){
   pressure    = bme.readPressure()/ 100.0F;
   tempamb     = mlx.readAmbientTempC();
   tempobj     = mlx.readObjectTempC();
+  tempsky     = tsky_calc(tempobj, tempamb);
+  dewpoint    = dewpoint_calc(temperature, humidity);
+// RULES    status true means SAFE!  false means UNSAFE!
+  if (tempamb > limit_tamb){
+    status_tamb = true;
+  }else{
+    status_tamb = false;    
+  }
+  if (humidity < limit_humid){
+    status_humid = true;
+  }else{
+    status_humid = false;    
+  }
+  if (tempsky < limit_tsky){
+    status_tsky = true;
+  }else{
+    status_tsky = false;    
+  }
+  if ((tempamb - dewpoint) > limit_dew){
+    status_dew = true;
+  }else{
+    status_dew = false;    
+  }
+  if (status_tamb && status_humid && status_tsky){
+    status_weather = true;
+    time2close = 125;
+  }else{
+    status_weather = false;
+    time2open = 1205;
+  }
+  time2open -= measureDelay;
+  if (time2open < 0) time2open = 0;
+  time2close -= measureDelay;
+  if (time2close < 0) time2close = 0;
 }
 
 void setup() {
@@ -171,35 +243,8 @@ void loop() {
   server.handleClient();
   if (millis() > lastTimeRan + measureDelay)  {   // read every measureDelay without blocking Webserver
     readSensors();
-    if(DEBUG) printBME();
     lastTimeRan = millis();
+    if (time2open > 0) time2open -= measureDelay;
+    if (time2close > 0) time2close -= measureDelay;
   }
-}
-
-void printMLX(){
-  Serial.print("MLX90614 sensor\n"); 
-  Serial.print("===============\n"); 
-  Serial.print("Ambient Temperature= "); 
-  Serial.print(mlx.readAmbientTempC()); Serial.println(" °C");
-  Serial.print("Object Temperature = "); 
-  Serial.print(mlx.readObjectTempC()); Serial.println(" °C"); 
-  Serial.println();
-}
-
-void printBME() {
-  Serial.print("BME280 sensor\n"); 
-  Serial.print("=============\n"); 
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" °C");
-    Serial.print("Pressure = ");
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(1013.25));
-    Serial.println(" m");
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-    Serial.println();
 }
